ac09238a9f2ad8a00cb67bb293ba39a71b229f53
Batsheva Rich <batsheva7424@gmail.com>
2019-08-27 11:40:38 +0300

    options page, did checkbox list, need to fix user input

--- project/src/pages/options/options.ts
+++ project/src/pages/options/options.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
 
 /**
  * Generated class for the OptionsPage page.
@@ -10,42 +10,54 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-options',
-  templateUrl: 'options.html',
+  selector: "page-options",
+  templateUrl: "options.html"
 })
 export class OptionsPage {
-  labels: Array<{name: string, probability: number}>;
+  labels: Array<{ name: string; probability: number; wanted: boolean }>;
   image: string;
   counter: number;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.image = ''; //need to be loaded from service
+    this.image = ""; //need to be loaded from service
     //need to be loaded from service
     this.labels = [];
     let i = 0;
-    for(; i < 10; i++) {//from service
-      
+    for (; i < 10; i++) {
+      //from service
+
       this.labels.push({
-        name: 'Item ' + i,
-        probability: Math.random()
+        name: "Item " + i,
+        probability: Math.random(),
+        wanted: true
       });
-      this.counter = i+1;
+      this.counter = i + 1;
     }
-  
   }
   itemClicked(e): void {
-      if(e.checked)
-      this.counter ++;
-      else this.counter--;
-     console.log(this.counter);
-}
+    if(!e.checked){
+      this.counter--;
+    }
+    else{
+        this.counter++;
+      }
+    console.log(e.value);
+    console.log(this.counter);
+  }
+  value = "";
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad OptionsPage');
+  addedLabel(e): void {
+    console.log(e);
+    this.labels.push({
+      name: e,
+      probability: 1,
+      wanted: true
+    });
+    this.counter = this.counter + 1;
+    console.log(this.counter);
+    this.value = "";
   }
 
-  
-
-
-
-
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad OptionsPage");
+  }
 }
