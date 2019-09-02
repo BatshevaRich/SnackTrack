b19a8bead73919e74672a9ca63aeec3c7e84cb61
Batsheva Rich <batsheva7424@gmail.com>
2019-09-02 11:50:45 +0300

    added file load to api, need to move to options page

--- project/src/pages/camera/camera.ts
+++ project/src/pages/camera/camera.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ApiPictureProvider } from '../../providers/api-picture/api-picture';
-import {ImageSnippet } from '../../app/classes/Image';
-import { Label } from '../../app/classes/Label';
-import { Observable } from 'rxjs';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { ApiPictureProvider } from "../../providers/api-picture/api-picture";
+import { ImageSnippet } from "../../app/classes/Image";
+import { Label } from "../../app/classes/Label";
+import { Observable } from "rxjs";
 /**
  * Generated class for the CameraPage page.
  *
@@ -13,56 +13,77 @@ import { Observable } from 'rxjs';
 
 @IonicPage()
 @Component({
-  selector: 'page-camera',
-  templateUrl: 'camera.html',
+  selector: "page-camera",
+  templateUrl: "camera.html",
   providers: [ApiPictureProvider]
 })
 export class CameraPage {
-  
   selectedFile: ImageSnippet;
   // public apPic:ApiPictureProvider;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public apPic:ApiPictureProvider ) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public apPic: ApiPictureProvider
+  ) {
     console.log(apPic);
   }
-  
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CameraPage');
+    console.log("ionViewDidLoad CameraPage");
   }
   loadedLabels: Label[];
- 
-  processFile($event):void {
+  fileToUpload: File = null;
+  urls: string[] = new Array();
+  processFile($event): void {
     const file: File = $event.target.files[0];
-    const reader = new FileReader();
-    var preview; 
-    reader.addEventListener('load', (event: any) => {
-      preview = document.getElementById('preview');
-      this.selectedFile = new ImageSnippet(event.target.result, file);
 
-      this.selectedFile.pending = true;
-      // this.imageService.uploadImage(this.selectedFile.file).subscribe(
-      //   (res) => {
-      //     this.onSuccess();
-      //   },
-      //   (err) => {
-      //     this.onError();
-      //   })
-      preview.src = reader.result; 
-    });
 
+
+
+    let _formData = new FormData();
+    this.fileToUpload = file;
+    _formData.append("file", this.fileToUpload);
+    var reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.urls.push(event.target.result);
+      console.log(event.target.result);
+    };
     reader.readAsDataURL(file);
+    console.log(JSON.stringify(_formData));
+    localStorage.setItem('loadedImage', JSON.stringify(_formData));
+
+    // this.InsertImages(_formData); //send the images' url to the server = in order to init the table
   }
-  getLabels(){
+  // this.imageService.uploadImage(this.selectedFile.file).subscribe(
+  //   (res) => {
+  //     this.onSuccess();
+  //   },
+  //   (err) => {
+  //     this.onError();
+  //   })
+  //     preview.src = reader.result;
+  //   });
+
+  //   reader.readAsDataURL(file);
+
+  InsertImages(_formData: FormData) {
+    debugger;
+    this.apPic.InsertImages(_formData).subscribe((res: any) => {
+      alert(res);
+    });
+  }
+
+  getLabels() {
     // this.apPic.GetLabelsForPicture(this.selectedFile.src);
   }
   private onSuccess() {
     this.selectedFile.pending = false;
-    this.selectedFile.status = 'ok';
+    this.selectedFile.status = "ok";
   }
 
   private onError() {
     this.selectedFile.pending = false;
-    this.selectedFile.status = 'fail';
-    this.selectedFile.src = '';
+    this.selectedFile.status = "fail";
+    this.selectedFile.src = "";
   }
-
 }
