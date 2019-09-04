47c1e1a6b9b2a1af00cf6444a2d278e41b840b66
Batsheva Rich <batsheva7424@gmail.com>
2019-09-04 19:56:09 +0300

    deleted 'hello-ionic' auto generated page

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule, ErrorHandler } from "@angular/core";
 import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
 import { MyApp } from "./app.component";
-import { HelloIonicPage } from "../pages/hello-ionic/hello-ionic";
 import { ItemDetailsPage } from "../pages/item-details/item-details";
 import { ListPage } from "../pages/list/list";
 import { OptionsPage } from "../pages/options/options";
@@ -20,7 +19,6 @@ import { MealProvider } from '../providers/meal/meal';
 @NgModule({
   declarations: [
     MyApp,
-    HelloIonicPage,
     ItemDetailsPage,
     ListPage,
     OptionsPage,
@@ -37,7 +35,6 @@ import { MealProvider } from '../providers/meal/meal';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HelloIonicPage,
     ItemDetailsPage,
     ListPage,
     OptionsPage,
