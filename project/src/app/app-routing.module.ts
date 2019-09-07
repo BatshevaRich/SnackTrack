030b0369bbe216c3bbc33ff3aa74dcce13a9c722
Batsheva Rich <batsheva7424@gmail.com>
2019-09-07 22:35:54 +0300

    removed event and day unnecessary pages

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -5,10 +5,8 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'options', loadChildren: './options/options.module#OptionsPageModule' },
-  { path: 'event', loadChildren: './event/event.module#EventPageModule' },
   { path: 'search', loadChildren: './search/search.module#SearchPageModule' },
   { path: 'camera', loadChildren: './camera/camera.module#CameraPageModule' },
-  { path: 'day', loadChildren: './day/day.module#DayPageModule' },
   { path: 'upload', loadChildren: './upload/upload.module#UploadPageModule' },
 
 ];
