61cbac95e0eddc46cc56f0661706da4ac43aeb0d
Batsheva Rich <batsheva7424@gmail.com>
2019-09-18 18:22:07 +0300

    fixed dependancies, will work

--- dal/dal/Model1.Designer.cs
+++ dal/dal/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\owner\Documents\GitHub\backend\dal\dal\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\owner\Documents\GitHub\SnackTrackBackend\dal\dal\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
