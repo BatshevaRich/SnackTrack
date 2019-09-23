1170fdb841178bafde81f3c7938b89e38c4525c4
ayalabarsheshet <45319556+ayalabarsheshet@users.noreply.github.com>
2019-09-23 13:09:38 +0300

    fixed app module, config

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -19,18 +19,17 @@ import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ViewDayMealPageModule } from './view-day-meal/view-day-meal.module';
 import {IonicStorageModule} from '@ionic/storage';
 // import { FilterPipe } from './pipes/filter.pipe';
-import { MbscModule } from '@mobiscroll/angular-lite';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [        MbscModule
-,
+  imports: [
     NgbModule, PipesModule,
     BrowserModule, BrowserAnimationsModule, HttpClientModule, CommonModule, IonicModule.forRoot(),IonicStorageModule.forRoot(),AppRoutingModule,ViewDayMealPageModule],
   providers: [
     StatusBar,
-    SplashScreen, HttpClientModule, Camera,
+    SplashScreen, HttpClientModule, Camera,NativeStorage,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
