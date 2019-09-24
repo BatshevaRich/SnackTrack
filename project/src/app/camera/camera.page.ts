8fb6da2f45edf0566d8e8b8d3bcbcce6cee547cf
Batsheva Rich <batsheva7424@gmail.com>
2019-09-25 01:09:18 +0300

    camera configs

--- project/src/app/camera/camera.page.ts
+++ project/src/app/camera/camera.page.ts
@@ -22,7 +22,12 @@ export class CameraPage implements OnInit {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
+      mediaType: this.camera.MediaType.PICTURE,
+      targetWidth: 900,
+      targetHeight: 600,
+      saveToPhotoAlbum: false,
+      allowEdit: true,
+      sourceType: 1
     };
 
     this.camera.getPicture(options).then((imageData) => {
